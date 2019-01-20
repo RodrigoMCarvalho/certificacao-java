be8875454c844e8fbbb3c1a2c768de87dd1216c7
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-01-20 19:56:38 -0200

    realizando exercicios

--- FatorialRecursividade.java
+++ FatorialRecursividade.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
+// 5! = 5x4x3x2x1=120
+
 class FatorialRecursividade {
-	
+
+	public static void main(String[] args){
+
+		Scanner ler = new Scanner(System.in);
+		int num, retorno;
+
+		System.out.print("Digite um numero: ");
+		num = ler.nextInt();
+		System.out.print(num + "!=");
+		retorno = Calculadora.fatorial(num);
+		System.out.print(retorno);
+	}
+}
+
+class Calculadora{	
+
+	public static int fatorial(int num){
+
+		if(num == 0) {
+			return 1;
+		}
+		return num * fatorial(num - 1); //método chama ele mesmo em loop até num for igual a zero
+	}
 }
